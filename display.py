6c1e7300a2b5b1f51e29d1ccc8f0b74580ab0a33
Ray <raypeaslee@gmail.com>
2025-07-05 13:05:49 -0400

    Create display.py
    to view current elo rankings

---
+++ display.py
@@ -0,0 +1,14 @@
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser(description="Ranks UFC fighters by the Elo-rating system")
+parser.add_argument("-n", "--number", dest="N", type=int, default=15, help="Number of fighters to be displayed (default: %(default)s)")
+args = parser.parse_args()
+
+display = pd.read_csv('display.csv')
+display = display.set_index('Fighter')
+pd.options.display.float_format = '{:.1f}'.format
+
+print(f"\nTop {args.N} fighters:")
+print(display.head(args.N))
+print(f"\n")
